60820205088bce84dda4959b07da9926e8a10e87
gitsomebit <>
2026-05-03 14:36:22 +0200

    feat(import): imported file extension

--- src/editor/project/project_database/content_database/content_database_import.go
+++ src/editor/project/project_database/content_database/content_database_import.go
@@ -101,10 +101,14 @@ func (r *ImportResult) ConfigPath() project_file_system.ConfigPath {
 	return r.ContentPath().ToConfigPath()
 }
 
-func (r *ImportResult) generateUniqueFileId(fs *project_file_system.FileSystem) string {
+func (r *ImportResult) generateUniqueFileId(fs *project_file_system.FileSystem, ext string) string {
 	defer tracing.NewRegion("ImportResult.generateUniqueFileId").End()
+	ext = strings.TrimSpace(ext)
+	if ext != "" && !strings.HasPrefix(ext, ".") {
+		ext = "." + ext
+	}
 	for {
-		r.Id = uuid.NewString()
+		r.Id = uuid.NewString() + ext
 		if _, err := fs.Stat(r.ContentPath().String()); err == nil {
 			continue
 		}

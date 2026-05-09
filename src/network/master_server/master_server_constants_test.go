5e38f30eac32c69cf20c87a252b40282c8f026fc
Brent Farris <1002223+BrentFarris@users.noreply.github.com>
2026-05-08 19:09:28 -0500

    Adding network tests

---
+++ src/network/master_server/master_server_constants_test.go
@@ -0,0 +1,125 @@
+/******************************************************************************/
+/* master_server_constants_test.go                                            */
+/******************************************************************************/
+/*                            This file is part of                            */
+/*                                KAIJU ENGINE                                */
+/*                          https://kaijuengine.com/                          */
+/******************************************************************************/
+/* MIT License                                                                */
+/*                                                                            */
+/* Copyright (c) 2023-present Kaiju Engine authors (AUTHORS.md).              */
+/* Copyright (c) 2015-present Brent Farris.                                   */
+/*                                                                            */
+/* May all those that this source may reach be blessed by the LORD and find   */
+/* peace and joy in life.                                                     */
+/* Everyone who drinks of this water will be thirsty again; but whoever       */
+/* drinks of the water that I will give him shall never thirst; John 4:13-14  */
+/*                                                                            */
+/* Permission is hereby granted, free of charge, to any person obtaining a    */
+/* copy of this software and associated documentation files (the "Software"), */
+/* to deal in the Software without restriction, including without limitation  */
+/* the rights to use, copy, modify, merge, publish, distribute, sublicense,   */
+/* and/or sell copies of the Software, and to permit persons to whom the      */
+/* Software is furnished to do so, subject to the following conditions:       */
+/*                                                                            */
+/* The above copyright notice and this permission notice shall be included in */
+/* all copies or substantial portions of the Software.                        */
+/*                                                                            */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS    */
+/* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                 */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.     */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY       */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT  */
+/* OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE      */
+/* OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                              */
+/******************************************************************************/
+
+package master_server
+
+import "testing"
+
+func TestConstants(t *testing.T) {
+	if MaxPasswordSize != 16 {
+		t.Errorf("MaxPasswordSize = %d, want 16", MaxPasswordSize)
+	}
+	if gameKeySize != 32 {
+		t.Errorf("gameKeySize = %d, want 32", gameKeySize)
+	}
+	if gameNameSize != 64 {
+		t.Errorf("gameNameSize = %d, want 64", gameNameSize)
+	}
+	if serversPerResponse != 10 {
+		t.Errorf("serversPerResponse = %d, want 10", serversPerResponse)
+	}
+	if addressMaxLen != 64 {
+		t.Errorf("addressMaxLen = %d, want 64", addressMaxLen)
+	}
+}
+
+func TestErrorConstants(t *testing.T) {
+	if ErrorNone != 0 {
+		t.Errorf("ErrorNone = %d, want 0", ErrorNone)
+	}
+	if ErrorIncorrectPassword != 1 {
+		t.Errorf("ErrorIncorrectPassword = %d, want 1", ErrorIncorrectPassword)
+	}
+	if ErrorServerDoesntExist != 2 {
+		t.Errorf("ErrorServerDoesntExist = %d, want 2", ErrorServerDoesntExist)
+	}
+}
+
+func TestRequestTypeConstants(t *testing.T) {
+	if RequestTypeRegisterServer != 0 {
+		t.Errorf("RequestTypeRegisterServer = %d, want 0", RequestTypeRegisterServer)
+	}
+	if RequestTypeUnregisterServer != 1 {
+		t.Errorf("RequestTypeUnregisterServer = %d, want 1", RequestTypeUnregisterServer)
+	}
+	if RequestTypePing != 2 {
+		t.Errorf("RequestTypePing = %d, want 2", RequestTypePing)
+	}
+	if RequestTypeServerList != 3 {
+		t.Errorf("RequestTypeServerList = %d, want 3", RequestTypeServerList)
+	}
+	if RequestTypeJoinServer != 4 {
+		t.Errorf("RequestTypeJoinServer = %d, want 4", RequestTypeJoinServer)
+	}
+}
+
+func TestResponseTypeConstants(t *testing.T) {
+	if ResponseTypeConfirmRegister != 0 {
+		t.Errorf("ResponseTypeConfirmRegister = %d, want 0", ResponseTypeConfirmRegister)
+	}
+	if ResponseTypeServerList != 1 {
+		t.Errorf("ResponseTypeServerList = %d, want 1", ResponseTypeServerList)
+	}
+	if ResponseTypeJoinServerInfo != 2 {
+		t.Errorf("ResponseTypeJoinServerInfo = %d, want 2", ResponseTypeJoinServerInfo)
+	}
+	if ResponseTypeClientJoinInfo != 3 {
+		t.Errorf("ResponseTypeClientJoinInfo = %d, want 3", ResponseTypeClientJoinInfo)
+	}
+	if ResponseTypeError != 4 {
+		t.Errorf("ResponseTypeError = %d, want 4", ResponseTypeError)
+	}
+}
+
+func TestResponseSize(t *testing.T) {
+	// Verify Response struct size is consistent
+	expectedSize := 1 + // Type
+		serversPerResponse*(gameNameSize+8+2+2) + // List
+		addressMaxLen + // Address
+		4 + // TotalList
+		1 // Error
+	if int(responseStructSize()) != expectedSize {
+		t.Errorf("Response struct size = %d, calculated = %d", responseStructSize(), expectedSize)
+	}
+}
+
+func responseStructSize() int {
+	return 1 + // Type byte
+		serversPerResponse*(gameNameSize+8+2+2) + // each ResponseServerList: name(64) + id(8) + max(2) + cur(2)
+		addressMaxLen + // Address byte array
+		4 + // TotalList uint32
+		1 // Error byte
+}
